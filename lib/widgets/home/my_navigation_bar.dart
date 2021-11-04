fbcee6d55c97c6d31bfdcae4461dc66093a70354
Jared <jaredclambert@gmail.com>
2021-11-03 22:33:20 -0600

    add safe area

--- lib/widgets/home/my_navigation_bar.dart
+++ lib/widgets/home/my_navigation_bar.dart
@@ -11,45 +11,49 @@ class MyNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final eventState = Provider.of<AddEventState>(context);
-    return Container(
-      padding: EdgeInsets.only(left: 50, right: 50, bottom: 7, top: 5),
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      decoration: BoxDecoration(
-          border:
-              Border(top: BorderSide(color: Colors.grey[200] ?? Colors.grey))),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: () => indexSetState(0),
-            icon: Icon(
-              Icons.home_outlined,
-              size: 35,
-              color: selectedIndex == 0 ? Color(0xffe85c3f) : Colors.grey[850],
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.only(left: 50, right: 50, bottom: 0, top: 5),
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        decoration: BoxDecoration(
+            border: Border(
+                top: BorderSide(color: Colors.grey[200] ?? Colors.grey))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () => indexSetState(0),
+              icon: Icon(
+                Icons.home_outlined,
+                size: 35,
+                color:
+                    selectedIndex == 0 ? Color(0xffe85c3f) : Colors.grey[850],
+              ),
             ),
-          ),
-          IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              eventState.clear();
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => AddEvent()));
-            },
-            icon: Icon(Icons.add_circle_outline,
-                size: 35, color: Colors.grey[850]), //Color(0xffe85c3f)),
-          ),
-          IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: () => indexSetState(1),
-            icon: Icon(
-              Icons.person_outline,
-              size: 35,
-              color: selectedIndex == 1 ? Color(0xffe85c3f) : Colors.grey[850],
+            IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                eventState.clear();
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddEvent()));
+              },
+              icon: Icon(Icons.add_circle_outline,
+                  size: 35, color: Colors.grey[850]), //Color(0xffe85c3f)),
             ),
-          ),
-        ],
+            IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () => indexSetState(1),
+              icon: Icon(
+                Icons.person_outline,
+                size: 35,
+                color:
+                    selectedIndex == 1 ? Color(0xffe85c3f) : Colors.grey[850],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
