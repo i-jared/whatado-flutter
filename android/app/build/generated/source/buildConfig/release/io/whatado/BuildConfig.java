e19f976e4cbfa1f1421c813d7eaacd2cb9f821b6
Jared Lambert <jaredclambert@gmail.com>
2022-08-16 14:08:31 -0600

    join button

--- android/app/build/generated/source/buildConfig/release/io/whatado/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/io/whatado/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "io.whatado";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 34;
-  public static final String VERSION_NAME = "1.2.5";
+  public static final int VERSION_CODE = 35;
+  public static final String VERSION_NAME = "1.2.6";
 }
